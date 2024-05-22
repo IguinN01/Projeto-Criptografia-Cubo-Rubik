2d730fbf57c4f3cd9844a5d9a3754d43a1ee1488
aline <156138287+pirezita@users.noreply.github.com>
2024-05-22 10:54:34 -0300

    latex da documentação do projeto

---
+++ clean.sh
@@ -0,0 +1,27 @@
+clear
+echo "Limpando arquivos..."
+rm *.aux
+rm *.bbl 
+rm *.blg 
+rm *.brf 
+rm *.dvi 
+rm *.ilg 
+rm *.ind 
+rm *.log 
+rm *.out 
+rm *.nav 
+rm *.snm 
+rm *.toc 
+rm *.idx 
+rm *.lof 
+rm *.lot 
+rm *.snm 
+rm *.bcf 
+rm *.run.xml 
+rm *.vrb,
+rm *.fdb_latexmk
+rm *.fls
+rm *.gz
+rm *.upa
+clear
+echo "Terminado!"
